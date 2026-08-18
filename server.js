fd228e0f9d49b3ed970bc155686d0755b84d4f5b
TeamEagleRp <mg7app@gmail.com>
2026-08-18 23:14:02 +0300

    Fix duplicate fs declaration

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const fs = require('fs');
 const multer = require('multer');
 
 // استدعاء البوت لتشغيله مع السيرفر بنفس الوقت (اختياري)
